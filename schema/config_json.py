5e0e4c996ef12b4cb6914c50f6ba1856f13d5501
Edson Rodrigo <63821150+Edson1337@users.noreply.github.com>
2024-07-02 08:52:28 -0300

    This is the JSON structure that will contain all the fields needed to summarize the results generated by the program.

---
+++ schema/config_json.py
@@ -0,0 +1,21 @@
+parsed_json = {
+    "performance_evaluation": {
+        "engine": "Sitespeed",
+        "project_name": "",
+        "app_name": "",
+        "url": "",
+        "performance_evaluation_id": "",
+        "metrics": [
+            "firstContentfulPaint",
+            "largestContentfulPaint",
+            "cumulativeLayoutShift",
+            "pageLoadTime",
+            "ttfb",
+            "maxPotentialFid",
+            "totalBlockingTime",
+            "requests",
+            "contentSize"
+        ],
+        "scenarios": []
+    }
+}
