9ca1f821296416e81b4106fca258fdcb125f3609
Edson Rodrigo <ed1337pi@hotmail.com>
2024-10-01 21:49:13 -0300

    Removal of a function that is no longer needed.

--- utils/settings_files_builder.py
+++ utils/settings_files_builder.py
@@ -1,20 +1,7 @@
-import os
 from pathlib import Path
 
 root_folder_path = Path("temp_scenario_settings").resolve()
 config_folder_path = root_folder_path / "configs"
-
-def rewrite_url_in_file(parsed_json):
-    parsed_json = parsed_json["performance_evaluation"]
-    passed_url = parsed_json["url"]
-    print(root_folder_path)
-
-    url_txt_file_path = root_folder_path / "urls.txt"
-    if os.path.exists(url_txt_file_path):
-        os.remove(url_txt_file_path)
-
-    with open(url_txt_file_path, 'w') as file:
-        file.write(passed_url)
 
 def delete_sitespeed_results(site_speed_result_path):
     delete_folder(site_speed_result_path)
