2a000dca2829b6dd679de51768ae545f66fca10d
Edson Rodrigo <ed1337pi@hotmail.com>
2024-10-01 21:20:52 -0300

    Update: Adjustment to suit the new script names. In addition, the logic for which script should be executed has been adjusted, and we now have the terms "before" and "after" for evaluation.

--- engine_run/sitespeed_runner.py
+++ engine_run/sitespeed_runner.py
@@ -1,27 +1,30 @@
 import os
 import subprocess
 
-script_ssr_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sitespeed_executer_ssr.sh')
-command_to_ssr = f'bash {script_ssr_path}'
-script_csr_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'sitespeed_executer_csr.sh')
-command_to_csr = f'bash {script_csr_path}'
 
 def execute_sitespeed_from_shell_script(parsed_json: dict):
-    print("Try running this project in CMD or PowerShell terminal!")
+    url = parsed_json["performance_evaluation"]["url"]
+
+    script_ssr_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'scripts', 'sitespeed_executer_baseline.sh')
+    script_csr_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'scripts', 'sitespeed_executer_comparative.sh')
+    
+    command_to_ssr = f'bash {script_ssr_path} {url}'
+    command_to_csr = f'bash {script_csr_path} {url}'
+
+    print("Starting Evaluation...")
     try:
-        # Criando o diretório antes de executar o script
         directory_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'baseline_to_statistical')
         os.makedirs(directory_path, exist_ok=True)
-        print(f'Diretório {directory_path} criado ou já existente.')
-
-        if "ssr" not in parsed_json["performance_evaluation"]["app_name"]:
-            print("Script para a analise com Compare iniciado")
+        print(f'Directory {directory_path} created or already existing.')
+        
+        if ("ssr" or "_before") not in parsed_json["performance_evaluation"]["app_name"]:
+            print("Script for analysis with Compare started")
             process = subprocess.Popen(command_to_csr, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
         else:
-            print("Script para a Baseline iniciado")
+            print("Script for Baseline started")
             process = subprocess.Popen(command_to_ssr, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-        # Imprimindo a saída em tempo real
         for line in iter(process.stdout.readline, b''):
             print(line.decode('utf-8').strip())
         
